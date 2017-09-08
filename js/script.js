8299440663be489f0aae7663dc6f1321264eee2f
Nathan Henniges <demonwolf@demonwolfdev.com>
2017-09-08 12:11:12 -0500

    Adding the abou.

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Fixs the menu so when link is clicked it would close the menu.
+$(function () {
+
+    $('#bs-example-navbar-collapse-1')
+        .on('shown.bs.collapse', function () {
+            $('#navbar-hamburger').addClass('hidden');
+            $('#navbar-close').removeClass('hidden');
+        })
+        .on('hidden.bs.collapse', function () {
+            $('#navbar-hamburger').removeClass('hidden');
+            $('#navbar-close').addClass('hidden');
+        });
+
+});
+$(document).on('click', '.navbar-collapse.in', function (e) {
+    if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+        $(this).collapse('hide');
+    }
+});
