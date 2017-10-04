45550b8f682200fa2ddbe620066505e2b8149d41
Nathan Henniges <demonwolf@demonwolfdev.com>
2017-10-04 14:08:59 -0500

    Added projects gallery page.

--- js/script.js
+++ js/script.js
@@ -47,3 +47,28 @@ $(document).ready(function () {
         $("#cookieConsent").fadeOut(200);
     });
 }); 
+// Project Gallery
+$(document).ready(function () {
+
+    $(".project_gallery_filter-button").click(function () {
+        var value = $(this).attr('data-filter');
+
+        if (value == "all") {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else {
+            //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+            //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.' + value).hide('3000');
+            $('.filter').filter('.' + value).show('3000');
+
+        }
+    });
+
+    if ($(".project_gallery_filter-button").removeClass("active")) {
+        $(this).removeClass("active");
+    }
+    $(this).addClass("active");
+
+});
