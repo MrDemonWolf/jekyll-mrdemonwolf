c86826df9b89785b4fbe4a337b80312fe8dafa33
Nathan Henniges <demonwolf@demonwolfdev.com>
2017-10-25 17:38:05 -0500

    Stuff. ANd restored the contact style to default to redo cleaning.

--- assets/_js/main.js
+++ assets/_js/main.js
@@ -32,43 +32,10 @@ $('.js-input').keyup(function () {
     }
 });
 $(document).ready(function () {
-    $(".fancybox").fancybox({
-        openEffect: "none",
-        closeEffect: "none"
-    });
-});
-
-
-$(document).ready(function () {
     setTimeout(function () {
         $("#cookieConsent").fadeIn(200);
     }, 4000);
     $("#closeCookieConsent, .cookieConsentOK").click(function () {
         $("#cookieConsent").fadeOut(200);
     });
-}); 
-// Project Gallery
-$(document).ready(function () {
-
-    $(".project_gallery_filter-button").click(function () {
-        var value = $(this).attr('data-filter');
-
-        if (value == "all") {
-            //$('.filter').removeClass('hidden');
-            $('.filter').show('1000');
-        }
-        else {
-            //$('.filter[filter-item="'+value+'"]').removeClass('hidden');
-            //$(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
-            $(".filter").not('.' + value).hide('3000');
-            $('.filter').filter('.' + value).show('3000');
-
-        }
-    });
-
-    if ($(".project_gallery_filter-button").removeClass("active")) {
-        $(this).removeClass("active");
-    }
-    $(this).addClass("active");
-
 });
